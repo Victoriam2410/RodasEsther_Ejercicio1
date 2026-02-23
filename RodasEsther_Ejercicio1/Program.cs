6dc9f76c1fefbec1400dae24e5fe7b91416c55bf
Victoriam2410 <estherrodas61@gmail.com>
2026-02-22 18:24:17 -0600

    Inicio del programa con declaracion de variables

--- RodasEsther_Ejercicio1/Program.cs
+++ RodasEsther_Ejercicio1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("cual es su Tipo de atención\r\n1. Emergencia\r\n2. Consulta\r\n3. Pediatría\r\n4. Traumatología");
+int option = int .Parse(Console.ReadLine());
+Console.WriteLine("ingrese la edad del paciente");
+int edad = int .Parse(Console.ReadLine());
+Console.WriteLine("ingrese la temperatura del paciente");
+int temperatura int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese la saturación de oxígeno del paciente");
+int oxigeno = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese el nivel de dolor (0–10) del paciente");
+int dolor = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese la presión sistólica del paciente");
+int presion = int.Parse(Console.ReadLine());
