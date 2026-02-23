1703929ae4a07ea3951cbdd3d21353f4a8312eff
Victoriam2410 <estherrodas61@gmail.com>
2026-02-22 18:30:23 -0600

    integracion del primer if y switch

--- RodasEsther_Ejercicio1/Program.cs
+++ RodasEsther_Ejercicio1/Program.cs
@@ -3,10 +3,18 @@ int option = int .Parse(Console.ReadLine());
 Console.WriteLine("ingrese la edad del paciente");
 int edad = int .Parse(Console.ReadLine());
 Console.WriteLine("ingrese la temperatura del paciente");
-int temperatura int.Parse(Console.ReadLine());
+int temperatura = int.Parse(Console.ReadLine());
 Console.WriteLine("ingrese la saturación de oxígeno del paciente");
 int oxigeno = int.Parse(Console.ReadLine());
 Console.WriteLine("ingrese el nivel de dolor (0–10) del paciente");
 int dolor = int.Parse(Console.ReadLine());
 Console.WriteLine("ingrese la presión sistólica del paciente");
 int presion = int.Parse(Console.ReadLine());
+if (temperatura >0 &&  presion > 0 && edad > 0 &&  dolor > 0 && oxigeno > 0 && option >=1 && option <=4)
+{
+    switch (option)
+    {
+        case 1:
+        break;
+    }
+}
